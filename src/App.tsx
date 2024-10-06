b17b34e27c4805b307d48a87848bab4af69a60b3
Raquel Santos <m-raquel@outlook.com.br>
2024-10-06 18:29:40 -0300

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routes'
+import { Analytics } from '@vercel/analytics/react'
+
 export function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Router />
     </BrowserRouter>
   )
