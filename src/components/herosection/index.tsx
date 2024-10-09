fb6f452efc09f7a4faad3e34a083a1d642d78163
Raquel Santos <m-raquel@outlook.com.br>
2024-10-08 23:54:55 -0300

    responsive for desktop

--- src/components/herosection/index.tsx
+++ src/components/herosection/index.tsx
@@ -14,46 +14,48 @@ export function HeroSection() {
     <section>
       <Menubar />
 
-      <div className="flex max-lg:flex-col max-lg:items-center gap-4 mb-6">
-        <h1 className="text-3xl font-light text-center max-w-[230px]">
-          IDE, CONVIDAI A TODOS PARA O BANQUETE
-        </h1>
-        <Leitura />
-        <p className="font-light text-center">
-          Com a força do Espírito, testemunhas de Cristo
-        </p>
-        <CTA />
+      <div className="grid min-[1024px]:grid-cols-2 justify-center gap-4">
+        <div className="flex flex-col items-center gap-4 mb-6">
+          <h1 className=" text-3xl font-light text-center max-w-[230px]">
+            IDE, CONVIDAI A TODOS PARA O BANQUETE
+          </h1>
+          <Leitura />
+          <p className="font-light text-center">
+            Com a força do Espírito, testemunhas de Cristo
+          </p>
+          <CTA />
 
-        <div className="flex flex-wrap justify-center items-start gap-1.5">
-          <img
-            src={cmLogo}
-            alt="Logo da Campanha Missionária"
-            className="h-14 shadow-md bg-contain rounded-md"
-          />
-          <img
-            src={pomLogo}
-            alt="Logo da POM"
-            className="h-14 bg-white rounded-md shadow-md"
-          />
-          <img
-            src={cnbbLogo}
-            alt="Logo da CNBB"
-            className="h-14 bg-white rounded-md shadow-md"
-          />
-          <img
-            src={dioceseLogo}
-            alt="Logo da CNBB"
-            className="h-14 bg-white rounded-md shadow-md"
-          />
-          <img
-            src={paroquiaLogo}
-            alt="Logo da Paróquia São Pedro e São Paulo Apóstolos"
-            className="h-14 bg-white rounded-md shadow-md"
-          />
+          <div className="flex flex-wrap justify-center items-start gap-1.5">
+            <img
+              src={cmLogo}
+              alt="Logo da Campanha Missionária"
+              className="h-14 shadow-md bg-contain rounded-md"
+            />
+            <img
+              src={pomLogo}
+              alt="Logo da POM"
+              className="h-14 bg-white rounded-md shadow-md"
+            />
+            <img
+              src={cnbbLogo}
+              alt="Logo da CNBB"
+              className="h-14 bg-white rounded-md shadow-md"
+            />
+            <img
+              src={dioceseLogo}
+              alt="Logo da CNBB"
+              className="h-14 bg-white rounded-md shadow-md"
+            />
+            <img
+              src={paroquiaLogo}
+              alt="Logo da Paróquia São Pedro e São Paulo Apóstolos"
+              className="h-14 bg-white rounded-md shadow-md"
+            />
+          </div>
         </div>
-      </div>
 
-      <ImagesHeroSection />
+        <ImagesHeroSection />
+      </div>
     </section>
   )
 }
