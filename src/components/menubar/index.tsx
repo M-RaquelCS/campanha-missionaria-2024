9ed535fa947aeca9ce0b4c5d59a42b57af47dc37
Raquel Santos <m-raquel@outlook.com.br>
2024-10-10 23:38:13 -0300

    fix the links

--- src/components/menubar/index.tsx
+++ src/components/menubar/index.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import logoImg from '/logo.jpg'
 import { Menu } from 'lucide-react'
 
 export function Menubar() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+  }, [location])
+
+  const handleNavigation = (path: string, hash: string) => {
+    navigate(path, { replace: true })
+    setTimeout(() => {
+      const element = document.querySelector(hash)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+      }
+    }, 0) // Timeout to ensure the page fully loads
+  }
+
   return (
     <nav className="flex items-center justify-between gap-y-3 mb-4 lg:mb-8 py-3 relative">
       <a href="/" className="bg-slate-200 rounded-xl">
@@ -14,24 +38,27 @@ export function Menubar() {
       </a>
 
       <div className="hidden lg:block">
-        <a
-          href="#about"
+        <button
+          type="button"
+          onClick={() => handleNavigation('/#about', '#about')}
           className="leading-10 underline-offset-4 px-2 py-[2px] lg:p-2.5 rounded-md hover:bg-sky-400/20"
         >
           Sobre
-        </a>
-        <a
-          href="#downloads"
+        </button>
+        <button
+          type="button"
+          onClick={() => handleNavigation('/#downloads', '#downloads')}
           className="leading-10 underline-offset-4 px-2 py-[2px] lg:p-2.5 rounded-md hover:bg-sky-400/20"
         >
           Downloads
-        </a>
-        <a
-          href="/programacao"
+        </button>
+        <button
+          type="button"
+          onClick={() => navigate('/programacao')}
           className="leading-10 underline-offset-4 px-2 py-[2px] lg:p-2.5 rounded-md hover:bg-sky-400/20"
         >
           Programação
-        </a>
+        </button>
       </div>
 
       <div className="lg:hidden">
@@ -48,32 +75,35 @@ export function Menubar() {
 
           <DropdownMenu.Portal>
             <DropdownMenu.Content
-              className="bg-white flex flex-col z-50 min-w-[8rem] overflow-hidden rounded-md bg-popover p-1 text-popover-foreground shadow-md"
+              className="bg-white flex flex-col items-start z-50 min-w-[8rem] overflow-hidden rounded-md bg-popover p-1 text-popover-foreground shadow-md"
               sideOffset={5}
             >
               <DropdownMenu.Item asChild>
-                <a
-                  href="#about"
-                  className="leading-10 underline-offset-4 px-2 py-[2px] rounded-md hover:bg-sky-400/20"
+                <button
+                  type="button"
+                  onClick={() => handleNavigation('/#about', '#about')}
+                  className="leading-10 underline-offset-4 px-2 py-[2px] w-full text-start rounded-md hover:bg-sky-400/20"
                 >
                   Sobre
-                </a>
+                </button>
               </DropdownMenu.Item>
               <DropdownMenu.Item asChild>
-                <a
-                  href="#downloads"
-                  className="leading-10 underline-offset-4 px-2 py-[2px] rounded-md hover:bg-sky-400/20"
+                <button
+                  type="button"
+                  onClick={() => handleNavigation('/#downloads', '#downloads')}
+                  className="leading-10 underline-offset-4 px-2 py-[2px] w-full text-start rounded-md hover:bg-sky-400/20"
                 >
                   Downloads
-                </a>
+                </button>
               </DropdownMenu.Item>
               <DropdownMenu.Item asChild>
-                <a
-                  href="/programacao"
-                  className="leading-10 underline-offset-4 px-2 py-[2px] rounded-md hover:bg-sky-400/20"
+                <button
+                  type="button"
+                  onClick={() => navigate('/programacao')}
+                  className="leading-10 underline-offset-4 px-2 py-[2px] w-full text-start rounded-md hover:bg-sky-400/20"
                 >
                   Programação
-                </a>
+                </button>
               </DropdownMenu.Item>
             </DropdownMenu.Content>
           </DropdownMenu.Portal>
